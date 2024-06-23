eeea7b2068f387d94a3d798c04fc0b9c2f54b950
rosu <mentitas@tuta.io>
2024-06-23 18:07:03 -0300

    El servidor ya puede mandar mails

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from cryptography.fernet import Fernet
+from mail_server import send_email
 import time
 
 # Consigo la clave de encriptación
@@ -29,11 +30,16 @@ class MyServer(BaseHTTPRequestHandler):
             dec_request = str(dec_request)[2:-1]
 
             # Transformo la request en un array
-            request = dec_request.split("/")
+            mail, note = dec_request.split("/")
+            ip = self.client_address[0]
 
-            self.wfile.write(bytes("<p> You are connecting from %s </p>" % (self.client_address[0]), "utf-8"))
-            self.wfile.write(bytes("<p>We'll send a mail to %s" % request[0], "utf-8"))
-            self.wfile.write(bytes("<p>saying '%s'"             % request[1], "utf-8"))
+            self.wfile.write(bytes("<p> You are connecting from %s </p>" % ip, "utf-8"))
+            self.wfile.write(bytes("<p>We'll send a mail to %s" % mail, "utf-8"))
+            self.wfile.write(bytes("<p>saying '%s'"             % note, "utf-8"))
+
+            # Descomentar la siguiente linea para que se mande un mail cuando se accede a la url
+            # send_email("mail-server@lachiwa.com", mail, "Han accedido tu honey token desde la IP " + ip, note)
+
 
         except:
             self.wfile.write(bytes("<p>Error, wrong token provided</p>", "utf-8"))
