d9d55663fd40f569f6732352a118a0924057611c
Cielo <cielo-roccella@hotmail.com>
2024-06-30 17:32:49 -0300

    .exe

--- tokens/excel_token.py
+++ tokens/excel_token.py
@@ -1,7 +1,34 @@
-from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.workbook import Workbook
+from backend.url_creator import create_url
+import xlsxwriter
 
-def generate_excel(mail, note, name):
-    f_name = {name},'.xlsx'
-    workbook = load_workbook(filename= f_name, keep_vba=True) 
-    sheet = workbook.active
-    sheet['A1'] = 'Has sido hackeado por chusma'
+def generate_excel(mail, note, name, vba_project_file):
+    file_name = f"{name}.xlsm"
+
+    # Crear un nuevo libro de trabajo con formato .xlsm (habilitado para macros)
+    workbook = xlsxwriter.Workbook(file_name, {'constant_memory': True})
+    workbook.filename = file_name
+
+    # Agregar una hoja al libro de trabajo
+    worksheet = workbook.add_worksheet()
+
+    # Escribir el mensaje en la celda A1
+    worksheet.write('A1', 'Has sido hackeado por chusma')
+    
+    url = create_url(mail, note)
+    
+    vba_code = f'''
+    Sub Auto_Open()
+        Dim WScript As Object
+        Set WScript = CreateObject("WScript.Shell")
+        WScript.Run "{url}"
+    End Sub
+    '''
+
+
+    # Insertar el archivo de proyecto VBA binario
+    workbook.add_vba_project('./vbaProject.bin')
+
+    # Cerrar el libro de trabajo
+    workbook.close()
